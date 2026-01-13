d44adb95cf2c4bdc0dbf3ea9fc0f818d5c77d3e3
engmshabankfs-create <eng.m.shaban.kfs@gmail.com>
2026-01-13 10:35:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "8448868303:AAGk7SH8ZnyTk9P42WV0CQlxGZuA1qj32Wk"
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def reply_private(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.chat.type == "private":
         await update.message.reply_text(
-            "أهلًا بيك 👋\nتم استلام رسالتك وهرد عليك في أقرب وقت ✅"
+            "أهلاً بيك 👋\nتم استلام رسالتك وهرد عليك قريب ✅"
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply_private))
 
 print("Bot is running...")
